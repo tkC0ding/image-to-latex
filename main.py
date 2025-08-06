cccf8fd84ffd23bd3bdf7fb03de6e21ce927538c
tkC0ding <tkjagani@gmail.com>
2025-08-06 17:30:24 -0400

    split the data into train, validation and test

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from datasets import load_dataset
+from datasets import load_dataset, DatasetDict
 
-ds = load_dataset("OleehyO/latex-formulas", "cleaned_formulas")
+ds = load_dataset("OleehyO/latex-formulas", "cleaned_formulas", split="train")
 
-print(ds)
+split_dataset = ds.train_test_split(test_size=0.2, seed=42)
+
+val_test_split = split_dataset["test"].train_test_split(test_size=0.5, seed=42)
+
+final_dataset = DatasetDict({
+    "train" : split_dataset["train"],
+    "validation" : val_test_split["train"],
+    "test" : val_test_split["test"]
+})
+
+print(final_dataset)
